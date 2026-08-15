34314f66909d9c059acdc5e881ef8608197e9c58
Andrés Felipe Restán Ospina <andresrestano@iediegoemisas.edu.co>
2026-08-15 17:32:46 -0500

    Fix: Ejercicio tres terminado

--- EjercicioDos2/calculos.py
+++ EjercicioDos2/calculos.py
@@ -1,10 +1,16 @@
 
 import math
 
-def VelIniMovil(VelFin, Aceleracion, Distancia):
+from main import Distancia
+
+
+def VelIniMovil(VelFin,Aceleracion, Distancia):
+   
     VelIni = math.sqrt((VelFin**2) - (2 * Aceleracion * Distancia))
     return VelIni
+print("La velocidad inicial es:", VelIniMovil)
 
-def VelIniAutomovil(VelFin, Aceleracion, Tiempo):
-    VelIni = VelFin - (Aceleracion * Tiempo)
+def VelIniAutomovil(VelFin,Aceleracion, Tiempo):
+    VelIni = Distancia / Tiempo - (Aceleracion * Tiempo/2)
     return VelIni
+print("La velocidad inicial del automóvil es:", VelIniAutomovil)
